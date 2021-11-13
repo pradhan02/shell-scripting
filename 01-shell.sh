cf999a69e1d1d4cb2fef656f619af769b240108a
meela <meelapradhan400@gmail.com>
2021-11-13 15:52:16 -0800

    Added She-Bang

---
+++ 01-shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# #! is called as She-Bang, It denotes the path of interpreter and also ensure the script executes with that interpreter if you execute the script with ./script instead of bash script.sh
+
+# Any line starts with # is a comment and those lines will be ignored during execution
+
+also
+ls -ltr
+
