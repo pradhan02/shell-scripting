16f4c05ffcb80ef3886bcce2393a211a9406955e
meela <meelapradhan400@gmail.com>
2021-11-15 13:47:41 -0800

    Added variable command.

---
+++ 03-variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## If we assign a name to a set of data, then that is variable
+# Syntax: VAR-DATA
+
+# Number
+a=100
+# String
+b=abc
+
+# In bash shell and also by default there are no data types, Shell considers everything as String
+
+# Access the data in shell using $ character prefixing the variable name, Or you can also access variables with ${}
+
+echo Value of a = $a
+echo Value of b = $b
+echo Value of a = ${a}
