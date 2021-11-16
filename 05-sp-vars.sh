d114453eb9c7d09008e4c61b2d2b65fcd5f050dd
meela <meelapradhan400@gmail.com>
2021-11-16 15:29:15 -0800

    Added practiced before execution in inputs

---
+++ 05-sp-vars.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#0 is script name
+echo 0 = $ 0
+#first argument to script
+echo 1 = $ 1
+# second argument to script
+echo 2 = $ 2
+
+## * and @ are giving all the argument
+echo "* = $*"
+echo "@ = $@"
+echo "# = $#"
+
+echo -e "your name = $1\nyour age = $2"
+
+
+
+
+
+
+
+
+
+
