beb6d0e4d88899007b29190c4cce363b2f446ae4
meela <meelapradhan400@gmail.com>
2021-11-16 17:17:29 -0800

    Added function command

---
+++ 07-functions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Functions should always declared before using it. same like variable
+## So that is the reason, Function me always find in starting of the scripts
+
+function abc() {
+}
+
+## Main Program
+abc
+
