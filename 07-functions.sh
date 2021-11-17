93be5ccb74528921e777cffb557234d0a2ba6e4f
meela <meelapradhan400@gmail.com>
2021-11-16 17:24:11 -0800

    Added function command

--- 07-functions.sh
+++ 07-functions.sh
@@ -3,8 +3,8 @@
 ## Functions should always declared before using it. same like variable
 ## So that is the reason, Function me always find in starting of the scripts
 
-function abc() {
-
+abc() {
+echo i am a function abc
 }
 
 
