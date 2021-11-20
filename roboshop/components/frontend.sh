e596a99ee54bd5a6daea24fd80f4a1bf78cb1273
meela <meelapradhan400@gmail.com>
2021-11-20 15:41:27 -0800

    Added commands in frontend

---
+++ roboshop/components/frontend.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+source components/common.sh
+
+Print "Installing Nginx"
+yum install nginx -y &>>$LOG
+Stat $?
+
+Print "Download Html Pages"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+Stat $?
+
+Print "Remove Old Html Pages"
+rm -rf /usr/share/nginx/html/* &>>$LOG
+Stat $?
+
+Print "Extract Frontend Archive"
+unzip -d /tmp/tmp/frontend.zip &>>@LOG
+Stat $?
+
+Print "Copy files to Nginx path"
+mv /tmp/frontend-main/static/* /usr/share/nginx/html/
+Stat $?
+
+Print "Copy Nginx roboshop Config file"
+cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+Stat $?
+
+Print "Enabling Nginx"
+systemctk enable nginx &>>$LOG
+Stat $?
+
+Print "Starting Nginx"
+systemctl restart nginx &>>$LOG
+Stat $?
+
+
+
+
+
+#systemctl restart nginx
