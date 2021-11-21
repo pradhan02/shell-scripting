88929ef13244539ba0140c4a7cb86ab52744cb1b
meela <meelapradhan400@gmail.com>
2021-11-21 13:51:19 -0800

    fixed bug

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -11,7 +11,7 @@ yum install -y mongodb-org &>>$LOG
 Stat $?
 
 Print "Update MongoDB Config"
-sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongodb.conf &>>$LOG
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG
 Stat $?
 
 Print "Start MongoDB"
