f981066e5301a9b0199a39f6795d0f99ff9afa03
meela <meelapradhan400@gmail.com>
2021-11-22 18:36:40 -0800

    Added cart components

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -8,14 +8,7 @@ MSPACE=$(cat $0 common.sh | grep ^Print | awk -F '"' '{print $2}' | awk '{ print
 COMPONENT_NAME=Catalogue
 COMPONENT=catalogue
 NODEJS
+CHECK_MONGO-FROM_APP
 
-Print "Checking DB connections from APP"
-sleep 5
-STAT=$(curl -s localhost:8080/health | jq .mongo)
-if [ "$STAT" == "true" ]; then
-  Stat 0
-else
-  Stat 1
-fi
 
 
--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
 source components/common.sh
-MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+MSPACE=$(cat $0 | grep Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
 COMPONENT_NAME=User
 COMPONENT=user
 
 NODEJS
-
-
-
-Print "Checking DB connections from APP"
-sleep 5
-STAT=$(curl -s localhost:8080/health | jq .mongo)
-if [ "$STAT" == "true" ]; then
-  Stat 0
-else
-  Stat 1
-fi
-
+CHECK_MONG_FROM_APP
 
 
 
