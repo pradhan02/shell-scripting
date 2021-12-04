c353d7e5481ba49adf7f9db808fd2866812900df
meela <meelapradhan400@gmail.com>
2021-12-04 13:21:51 -0800

    added commands in ansible

---
+++ 09-loops.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Loops are two major commands , while & for
+# While loop works on Expressions that we used in if statements
+
+a=10
+while [ $a -gt 0 ]; do
+  echo While Loop
+   sleep 0.5
+   a=$(($a-1))
+   done
+
+#syntax : for var in items ; do commands ; done
+
+for fruit in apple banana orange peach ; do
+  echo Fruit Name = $fruit
+done
+
+echo -n "Checking Connection on Port 22 for host $1"
+while true ; do
+   nc -w 1 -z $1 22 &>/dev/null
+   if [ $? -eq 0 ]; then
+   break
+   fi
+   echo -n "."
+done
+
+# break command will help come out of the loop
